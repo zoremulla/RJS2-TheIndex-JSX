fea97bda38f0d91f4e825b8a9dd43e03cea521f2
Octowl <alsaff1987@gmail.com>
2019-01-28 20:38:56 +0300

    More tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import { mount } from "enzyme";
 import toJSON from "enzyme-to-json";
 
+import authors from "../data";
+
 import App from "../App";
 
 describe("<App />", () => {
@@ -20,12 +22,31 @@ describe("<App />", () => {
     expect(wrapper.find(".card").length).toBe(4);
   });
 
+  it("renders different authors", () => {
+    const wrapper = mount(<App />);
+    const cards = wrapper
+      .find(".card")
+      .map(card => card.find(".card-title").text());
+    expect(new Set(cards).size).toEqual(cards.length);
+  });
+
   it("has the correct content in each author card", () => {
     const wrapper = mount(<App />);
-    wrapper.find(".card").forEach(card => {
-      expect(card.find("img").exists()).toBe(true);
-      expect(card.find(".card-title").exists()).toBe(true);
-      expect(card.find(".card-text").text()).toContain("books");
+    wrapper.find(".card").forEach((card, idx) => {
+      const image = card.find("img");
+      const authorName = card.find(".card-title");
+      const books = card.find(".card-text");
+      const author = authors[idx];
+
+      expect(image.exists()).toBe(true);
+      expect(authorName.exists()).toBe(true);
+      expect(books.exists()).toBe(true);
+
+      expect(image.props().src).toBe(author.imageUrl);
+      expect(authorName.text()).toContain(
+        `${author.first_name} ${author.last_name}`
+      );
+      expect(books.text()).toContain(`${author.books.length} books`);
     });
   });
 });
