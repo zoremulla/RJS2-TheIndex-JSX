2320e573f1276b3d94c7cffa60b5128d669a73b9
Octowl <alsaff1987@gmail.com>
2019-01-28 17:57:06 +0300

    More comrehensive tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { mount } from "enzyme";
 import toJSON from "enzyme-to-json";
 
@@ -7,6 +7,25 @@ import App from "../App";
 describe("<App />", () => {
   it("renders correctly", () => {
     const wrapper = mount(<App />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
+    expect(wrapper.find("#app").exists()).toBe(true);
+  });
+
+  it("is not just an empty div", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find("#app").children().length).toBe(1);
+  });
+
+  it("renders the right number of authors", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(".card").length).toBe(4);
+  });
+
+  it("has the correct content in each author card", () => {
+    const wrapper = mount(<App />);
+    wrapper.find(".card").forEach(card => {
+      expect(card.find("img").exists()).toBe(true);
+      expect(card.find(".card-title").exists()).toBe(true);
+      expect(card.find(".card-text").text()).toContain("books");
+    });
   });
 });
